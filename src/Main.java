7e754b2f03cd99b79544fbac7eb142719d796098
ctysingh <chaitanyasingh@gmail.com>
2022-11-24 11:38:41 -0500

    created collection of dishes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Dish> dishes = Arrays.asList(
+                new Dish("pork", false,800, Type.MEAT),
+                new Dish(("beef", false, 700, Type.MEAT)),
+                new Dish("chicken", false, 400, Type.MEAT),
+                new Dish("french fries", true, 400, Type.OTHER),
+                new Dish("rice", true, 300, Type.OTHER),
+                new Dish("seasoned fruit", true, 200, Type.OTHER),
+                new Dish("pizza", true, 700, Type.OTHER),
+                new Dish("prawns", false, 600, Type.FISH),
+                new Dish("salmom", false, 450, Type.FISH),
+        );
+
+
+
     }
 }
